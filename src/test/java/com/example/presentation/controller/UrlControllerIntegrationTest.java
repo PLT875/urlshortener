21c08a5668c0dd1e4daf2609648dae6eb06ac3dd
Peter Tran <pltran875@gmail.com>
2025-03-01 09:15:59 +0000

    Handling collisions with tests

--- src/test/java/com/example/presentation/controller/UrlControllerIntegrationTest.java
+++ src/test/java/com/example/presentation/controller/UrlControllerIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.example.presentation.controller;
 
+import com.example.persistence.UrlRepository;
+import com.example.persistence.entity.UrlEntity;
 import com.example.testingutil.BaseIntegrationTest;
 import io.restassured.response.Response;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
 
 import static com.example.presentation.controller.UrlControllerFixtures.createShortUrl;
 import static io.restassured.RestAssured.given;
@@ -16,8 +22,12 @@ import static org.apache.http.HttpStatus.SC_NO_CONTENT;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class UrlControllerIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private UrlRepository urlRepository;
 
     @Test
     void whenCreateShortUrl_thenCreated_AndIsIdempotent() {
@@ -55,6 +65,28 @@ public class UrlControllerIntegrationTest extends BaseIntegrationTest {
             body("key", equalTo(key)).
             body("long_url", equalTo(longUrl)).
             body("short_url", equalTo(shortUrl));
+    }
+
+    @Test
+    void whenCreateShortUrl_withCollisionHandling_thenCreated() {
+        String url = "http://www.amazon.com";
+        String conflictedId1 = DigestUtils.sha256Hex(url).substring(0, 7);
+        String conflictedId2 = DigestUtils.sha256Hex(url + 1).substring(0, 7);
+        Set<UrlEntity> conflictedUrlEntities = Set.of(
+            new UrlEntity(conflictedId1, "http://www.amazon.co.uk"),
+            new UrlEntity(conflictedId2, "http://www.amazon.de")
+        );
+        urlRepository.saveAll(conflictedUrlEntities);
+
+        Response createShortUrlResponse = createShortUrl(url);
+        String key = createShortUrlResponse.getBody().jsonPath().getString("key");
+        String longUrl = createShortUrlResponse.getBody().jsonPath().getString("long_url");
+        String shortUrl = createShortUrlResponse.getBody().jsonPath().getString("short_url");
+        assertAll(
+            () -> assertThat(key).isNotEqualTo(conflictedId1),
+            () -> assertThat(key).isNotEqualTo(conflictedId2),
+            () -> assertThat(longUrl).isEqualTo(url),
+            () -> assertThat(shortUrl).isEqualTo("http://localhost:8888/" + key));
     }
 
     @Test
